7b4459c9ddd5824e17ee890247ec0869b396a5e4
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-08-28 10:32:28 +0530

    Refactor B. Effective Approach solution for clarity and maintainability

--- B_Effective_Approach.cpp
+++ B_Effective_Approach.cpp
@@ -1,7 +1,54 @@
 #include <iostream> 
+#include <vector>
 using namespace std;
 
+// This program compares two approaches to counting occurrences of elements in a vector.
+
 int main(){
-    int num;
-    cin >> num;
+    int n;
+    cin >> n;
+    vector<int> a;
+    for(int i=0; i<n; i++){
+        int x;
+        cin >>x;
+        a.push_back(x);
+    }
+
+    int b;
+    cin >> b;
+
+    long long v = 0;
+    long long p = 0;
+
+ 
+    for(int i=0; i<b; i++){
+        int x;
+        cin >> x;
+        
+        
+        //Vasya's approach
+        for(int j=0; j<n; j++){
+            v++;
+            if(a[j] == x){
+                break;
+            }
+        }
+
+        // Petya's approach
+        for(int j=n-1; j>=0; j--){
+            p++;
+            if(a[j] == x){
+                break;
+            }
+        }
+    }
+
+    vector<long long> ans;
+    ans.push_back(v);
+    ans.push_back(p);
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    return 0;
+
 }
